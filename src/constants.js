cf08fc59570ca97d5306deab1c0bb4b0c3d18025
Alex Xandra Albert Sim <bertzzie@gmail.com>
2016-12-06 14:54:37 +0700

    Updated links

--- src/constants.js
+++ src/constants.js
@@ -1,15 +1,28 @@
 export const GA_PROPERTY_ID = 'UA-9801603-6'
 
 let hostname = 'https://m-staging.tokopedia.com'
+let sites = {
+  Pulsa: 'https://pulsa-staging.tokopedia.com',
+  Tiket: 'https://tiket-staging.tokopedia.com'
+}
 let gtmContainerID = 'GTM-T8MQ8Z'
 
 if (__TEST__) {
   hostname = 'https://m-staging.tokopedia.com'
+  sites = {
+    Pulsa: 'https://pulsa-staging.tokopedia.com',
+    Tiket: 'https://tiket-staging.tokopedia.com'
+  }
   gtmContainerID = 'GTM-T8MQ8Z'
 } else if (__PROD__) {
   hostname = 'https://m.tokopedia.com'
+  sites = {
+    Pulsa: 'https://pulsa.tokopedia.com',
+    Tiket: 'https://tiket.tokopedia.com'
+  }
   gtmContainerID = 'GTM-WD6SLP'
 }
 
 export const HOSTNAME = hostname
+export const SITES = sites
 export const GTM_CONTAINER_ID = gtmContainerID
