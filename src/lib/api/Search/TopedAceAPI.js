217120d27acb29c06a7d2d31818b8597e399bc08
ruwhan <ruwhan@outlook.com>
2016-12-08 06:10:11 +0700

    propose const instace of static variable

--- src/lib/api/Search/TopedAceAPI.js
+++ src/lib/api/Search/TopedAceAPI.js
@@ -1,6 +1,17 @@
 import TopedAPI from 'lib/api/TopedAPI'
 import config from 'lib/api/config'
 
+const aceServices = {
+  SearchCatalog: `https://${config.Ace.Hostname}/search/v1/catalog`,
+  SearchShop: `https://${config.Ace.Hostname}/search/v1/shop`,
+  SpellCheck: `https://${config.Ace.Hostname}/v1/products/spellcheck`,
+  PriceRange: `https://${config.Ace.Hostname}/v1/price_range`, // /v1/price_range/[catalog_id]
+  CatalogShopList: `https://${config.Ace.Hostname}/search/v1/catalog/product`,
+  PopularSearch: `https://${config.Ace.Hostname}/v1/popular_search`,
+  RecentSearch: `https://${config.Ace.Hostname}/recent_search/v1`,
+  DynamicAttributes: `https://${config.Ace.Hostname}/v1/dynamic_attributes`,
+}
+
 class TopedAceAPI {
   static URL = {
     Universe: `https://${config.Ace.Hostname}/universe/v2`,
