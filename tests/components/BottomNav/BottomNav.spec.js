349b6f1d0d8d39aca345622a76f3121e16f975b3
darrylsepeda <darryl@thereal-website.com>
2016-12-14 18:29:25 +0700

    f_darryl_test_catalogcategory

--- tests/components/BottomNav/BottomNav.spec.js
+++ tests/components/BottomNav/BottomNav.spec.js
@@ -5,10 +5,12 @@ import { shallow } from 'enzyme';
 import BottomNav from 'components/BottomNav';
 
 describe('(Component) BottomNav', () => {
+  const wrapper = shallow(<BottomNav />);
   it('should has four bottom-nav__grid element', () => {
-    const wrapper = shallow(<BottomNav />);
-    expect(wrapper.find('.bottom-nav__grid').length).to.be.least(1);
-    expect(wrapper.find('.bottom-nav__link').length).to.be.least(1);
+    expect(wrapper.find('.bottom-nav__grid').length).to.be.least(4);
+    expect(wrapper.find('.bottom-nav__link').length).to.be.least(4);
+  });
+  it('should contains these four elements', () => {
     expect(wrapper.contains(<span className='u-mx-auto'>Home</span>)).to.be.true;
 	expect(wrapper.contains(<span className='u-mx-auto'>Hotlist</span>)).to.be.true;
 	expect(wrapper.contains(<span>Wishlist</span>)).to.be.true;
