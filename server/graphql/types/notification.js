b97231154b98c0fc50d03260c7abbebbfc242d37
Alex Xandra Albert Sim <bertzzie@gmail.com>
2016-12-16 10:23:00 +0700

    Notification empty handling

--- server/graphql/types/notification.js
+++ server/graphql/types/notification.js
@@ -9,7 +9,7 @@ const NotifData = require('./notif-data')
 const NotificationType = new GraphQLObjectType({
   name: 'Notification',
   fields: {
-    status: { type: GraphQLString },
+    status: { type: new GraphQLNonNull(GraphQLString) },
     data: { type: new GraphQLNonNull(NotifData) }
   }
 })
