11fa7bd2b514ef25c62665fe77f4411fa1c75266
Arian Pradana <arian.raptor@gmail.com>
2016-12-20 10:56:05 +0700

    Add props in header to support tab whether shown or not in various pages

--- src/components/SearchInputOld/SearchInputOld.js
+++ src/components/SearchInputOld/SearchInputOld.js
@@ -70,7 +70,7 @@ class SearchInputOld extends Component {
 
     let resultAutoComplete = (
       <div className={finalClassName}>
-        <div className='u-pt0 u-pb1'>
+        <div className='u-pt0 u-pb2'>
           <form action='/search' method='get' className='u-relative'>
             <input type='hidden' name='st' defaultValue='product' />
             <label htmlFor='search_input' className='u-hide'>Search</label>
