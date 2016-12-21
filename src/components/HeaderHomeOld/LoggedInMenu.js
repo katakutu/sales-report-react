d166b462c16b0319f83136fec1cc434e3fa2e9a5
Alex Xandra Albert Sim <bertzzie@gmail.com>
2016-12-21 16:28:29 +0700

    Tweak my shop

--- src/components/HeaderHomeOld/LoggedInMenu.js
+++ src/components/HeaderHomeOld/LoggedInMenu.js
@@ -86,8 +86,8 @@ class LoggedInMenu extends Component {
     let topupLink = `${SITES['Pulsa']}/saldo/?utm_source=mobile&utm_medium=link&utm_campaign=top%20up%20saldo`
 
     let shopSection = (this.props.shop['shop_id'] === 'ERROR FAIL' || this.props.shop['shop_id'] === null) ? (
-      <a href={`${HOSTNAME}/myshop.pl`}>
-        <div className='drawer__menu-shop u-clearfix'>
+      <div className='drawer__menu-shop u-clearfix'>
+        <a href={`${HOSTNAME}/myshop.pl`}>
           <div className='u-left'>
             <img className='drawer__menu-icon' src={addShop} alt='tokopedia' />
           </div>
@@ -96,8 +96,9 @@ class LoggedInMenu extends Component {
               { lang[l]['Open Shop'] }
             </div>
           </div>
-        </div>
-      </a>) : (
+        </a>
+      </div>
+      ) : (
         <a href={`${HOSTNAME}/${this.props.shop['domain']}`}>
           <div className='drawer__menu-shop u-clearfix'>
             <div className='u-left'>
@@ -212,7 +213,7 @@ class LoggedInMenu extends Component {
           <div className='drawer__user-topup'>
             <a href={topupLink} className='drawer__btn-topup'>Top Up Saldo</a>
           </div>
-          {shopSection}
+
           <div className='drawer__menu'>
             <a href='/'>
               <img className='drawer__menu-icon' src={homeIcon} alt='tokopedia' />
@@ -332,6 +333,8 @@ class LoggedInMenu extends Component {
               }</span>
             </a>
           </div>
+
+          { shopSection }
         </div>
       </div>
     )
