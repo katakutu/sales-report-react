05b0407471bcb8eceae912c8881fb110c4c3a441
Alex Xandra Albert Sim <bertzzie@gmail.com>
2016-12-22 13:01:21 +0700

    Added hot product list graphql query

--- server/graphql/queries/hotlist/hotlist.js
+++ server/graphql/queries/hotlist/hotlist.js
@@ -1,14 +1,28 @@
-const { GraphQLString } = require('graphql')
+const { GraphQLInt } = require('graphql')
 
 const HotlistsType = require('../../types/hotlists')
-const getHotlists = require('../../models/hotlist')
+const { getHotProductHome, getHotProductList } = require('../../models/hotlist')
 
-const HotlistsQuery = {
+const HotProductListsQuery = {
   type: HotlistsType,
-  args: { action: { type: GraphQLString } },
+  args: {
+    page: { type: GraphQLInt },
+    per_page: { type: GraphQLInt }
+  },
   resolve: function (_, args) {
-    return getHotlists(args.action)
+    return getHotProductList(args.page, args.per_page)
   }
 }
 
-module.exports = HotlistsQuery
+const HotProductHomeQuery = {
+  type: HotlistsType,
+  args: { },
+  resolve: function (_, args) {
+    return getHotProductHome()
+  }
+}
+
+module.exports = {
+  'hot_product_home': { 'hot_product_home': HotProductHomeQuery },
+  'hot_product_list': { 'hot_product_list': HotProductListsQuery }
+}
