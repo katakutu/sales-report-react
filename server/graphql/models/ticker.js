1021df8b936889a2f694cfbdf3812af018ebd0ba
Alex Xandra Albert Sim <bertzzie@gmail.com>
2016-12-23 10:07:37 +0700

    Fix ticker problem

--- server/graphql/models/ticker.js
+++ server/graphql/models/ticker.js
@@ -1,3 +1,4 @@
+const TopedAuthAPI = require('./../../api-consumer/api/Auth/TopedAuthAPI')
 const TopedMojitoAPI = require('./../../api-consumer/api/Search/TopedMojitoAPI')
 
 const EMPTY_TICKER = {
@@ -5,20 +6,31 @@ const EMPTY_TICKER = {
   tickers: []
 }
 
-function getTicker () {
+function getTicker (context) {
+  let userID = Promise.resolve(0)
+  if (context && context.session.oauth) {
+    const tType = context.session.oauth.token['token_type']
+    const token = context.session.oauth.token['access_token']
+
+    const authConsumer = new TopedAuthAPI(token, tType)
+    userID = authConsumer.getUserInfo().then(user => user['user_id'])
+  }
+
   const api = new TopedMojitoAPI()
 
-  return api.getTickers(0, 50, 'desktop', 'data_source_filter').then(response => {
-    if (!response['data']) {
-      return EMPTY_TICKER
-    }
+  return userID.then(uid => {
+    return api.getTickers(uid, 50, 'desktop', 'data_source_filter').then(response => {
+      if (!response['data']) {
+        return EMPTY_TICKER
+      }
 
-    const responseMeta = response['meta']
-    const resultMeta = (Object.keys(responseMeta).length > 0 && responseMeta.constructor === Object)
-      ? responseMeta
-      : { total_data: response['data']['tickers'].length }
+      const responseMeta = response['meta']
+      const resultMeta = (Object.keys(responseMeta).length > 0 && responseMeta.constructor === Object)
+        ? responseMeta
+        : { total_data: response['data']['tickers'].length }
 
-    return { meta: resultMeta, tickers: response['data']['tickers'] }
+      return { meta: resultMeta, tickers: response['data']['tickers'] }
+    })
   })
 }
 
