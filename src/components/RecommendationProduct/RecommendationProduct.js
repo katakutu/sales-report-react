19ff71a7ca51ff5105aff6708dc2b7e227b9e639
Arian Pradana <arian.raptor@gmail.com>
2017-01-03 13:57:05 +0700

    Fix code for new hotlist and toppicks

--- src/components/RecommendationProduct/RecommendationProduct.js
+++ src/components/RecommendationProduct/RecommendationProduct.js
@@ -1,37 +1,47 @@
 import React from 'react'
 import './RecommendationProduct.scss'
+import TextHeader from '../../components/TextHeader'
+import RecommendationProductPlaceholder from './assets/recommendation-product-placeholder.png'
 
 export const RecommendationProduct = () => (
-  <div className='u-clearfix recommendation-product-container'>
-    <h2 className='text-header'>Yang lagi Hot</h2>
-    <div className='recommendation-product__contents'>
-      <div className='u-col u-col-6 recommendation-product__box'>
-        <div className='recommendation-product__box-content'>
-          <a href='#'>
-            <img src='http://placehold.it/720x520' alt='' className='recommendation-product__img' />
-            <div className='recommendation-product__content-desc'>
-              <div className='title u-truncate'>Modem Mi-Fi</div>
-              <div className='startto'>Mulai dari <span className='price'>Rp. 200rb</span></div>
-            </div>
+  <div className='u-clearfix'>
+    <TextHeader textType={2} injectClassName='recommendation-product__title'>
+      Yang lagi Hot
+    </TextHeader>
+    <div className='recommendation-product-container'>
+      <div className='recommendation-product__contents'>
+        <div className='u-col u-col-6 recommendation-product__box'>
+          <div className='recommendation-product__box-content'>
+            <a href='#'>
+              <img src={RecommendationProductPlaceholder} alt='' className='recommendation-product__img' />
+              <div className='recommendation-product__content-desc'>
+                <div className='recommendation-product__content-title u-truncate'>Modem Mi-Fi</div>
+                <div className='recommendation-product__content-startto'>
+                  Mulai dari <span className='recommendation-product__content-price'>Rp. 200rb</span>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className='u-col u-col-6 recommendation-product__box'>
+          <div className='recommendation-product__box-content'>
+            <a href='#'>
+              <img src={RecommendationProductPlaceholder} alt='' className='recommendation-product__img' />
+              <div className='recommendation-product__content-desc'>
+                <div className='recommendation-product__content-title u-truncate'>Modem Mi-Fi</div>
+                <div className='recommendation-product__content-startto'>
+                  Mulai dari <span className='recommendation-product__content-price'>Rp. 200rb</span>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className='u-clearfix' />
+        <div className='recommendation-product__see-all'>
+          <a className='recommendation-product__see-all-link' href='https://www.tokopedia.com/toppicks/'>
+            Lihat Semua <i className='promo-spacer__icon promo-spacer__icon--arrow' />
           </a>
         </div>
-      </div>
-      <div className='u-col u-col-6 recommendation-product__box'>
-        <div className='recommendation-product__box-content'>
-          <a href='#'>
-            <img src='http://placehold.it/720x520' alt='' className='recommendation-product__img' />
-            <div className='recommendation-product__content-desc'>
-              <div className='title u-truncate'>Modem Mi-Fi</div>
-              <div className='startto'>Mulai dari <span className='price'>Rp. 200rb</span></div>
-            </div>
-          </a>
-        </div>
-      </div>
-      <div className='u-clearfix' />
-      <div className='recommendation-product__see-all'>
-        <a className='recommendation-product__see-all-link' href='https://www.tokopedia.com/toppicks/'>
-          Lihat semua <i className='promo-spacer__icon promo-spacer__icon--arrow' />
-        </a>
       </div>
     </div>
   </div>
--- src/components/Toppicks/Toppicks.js
+++ src/components/Toppicks/Toppicks.js
@@ -1,60 +1,72 @@
 import React from 'react'
 import './Toppicks.scss'
+import TextHeader from '../../components/TextHeader'
+import ToppicksPlaceholder from './assets/toppicks-placeholder.png'
+import ToppicksBannerPlaceholder from './assets/toppicks-banner-placeholder.png'
 
 export const Toppicks = () => (
-  <div className='u-clearfix toppicks-container'>
-    <h2 className='text-header'>Top Picks</h2>
-    <div className='toppicks__contents'>
-      <div className='toppicks__row u-clearfix'>
-        <div className='u-col u-col-6 toppicks__box'>
-          <a href='#'>
-            <div className='toppicks__banner'>
-              <img src='http://placehold.it/720x720' alt='' className='toppicks__img' />
+  <div className='u-clearfix'>
+    <TextHeader textType={2} injectClassName='toppicks__title'>
+      Top Picks
+    </TextHeader>
+    <div className='u-clearfix toppicks-container'>
+      <div className='toppicks__contents'>
+        <div className='toppicks__row u-clearfix'>
+          <div className='u-col u-col-6 toppicks__box'>
+            <a href='#'>
+              <div className='toppicks__banner'>
+                <img src={ToppicksBannerPlaceholder} alt='' className='toppicks__img' />
+              </div>
+            </a>
+          </div>
+          <div className='u-col u-col-6 toppicks__box'>
+            <div className='toppicks__box-content'>
+              <a href='#'>
+                <img src={ToppicksPlaceholder} alt='' className='toppicks__img' />
+                <div className='toppicks__content-desc'>
+                  <div className='toppicks__content-title u-truncate'>Munafie Slimming Pants</div>
+                  <div className='toppicks__content-startto u-truncate'>Mulai dari
+                    <span className='toppicks__content-price'>Rp. 350rb</span>
+                  </div>
+                </div>
+              </a>
             </div>
+          </div>
+        </div>
+
+        <div className='toppicks__row u-clearfix'>
+          <div className='u-col u-col-6 toppicks__box'>
+            <div className='toppicks__box-content'>
+              <a href='#'>
+                <img src={ToppicksPlaceholder} alt='' className='toppicks__img' />
+                <div className='toppicks__content-desc'>
+                  <div className='toppicks__content-title u-truncate'>Munafie Slimming Pants</div>
+                  <div className='toppicks__content-startto u-truncate'>Mulai dari
+                    <span className='toppicks__content-price'>Rp. 350rb</span>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div className='u-col u-col-6 toppicks__box'>
+            <div className='toppicks__box-content'>
+              <a href='#'>
+                <img src={ToppicksPlaceholder} alt='' className='toppicks__img' />
+                <div className='toppicks__content-desc'>
+                  <div className='toppicks__content-title u-truncate'>Munafie Slimming Pants</div>
+                  <div className='toppicks__content-startto u-truncate'>Mulai dari
+                    <span className='toppicks__content-price'>Rp. 350rb</span>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className='toppicks__see-all'>
+          <a className='toppicks__see-all-link' href='https://www.tokopedia.com/toppicks/'>
+            Lihat Semua <i className='promo-spacer__icon promo-spacer__icon--arrow' />
           </a>
         </div>
-        <div className='u-col u-col-6 toppicks__box'>
-          <div className='toppicks__box-content'>
-            <a href='#'>
-              <img src='http://placehold.it/720x520' alt='' className='toppicks__img' />
-              <div className='toppicks__content-desc'>
-                <div className='title u-truncate'>Munafie Slimming Pants</div>
-                <div className='startto u-truncate'>Mulai dari <span className='price'>Rp. 350rb</span></div>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div className='toppicks__row u-clearfix'>
-        <div className='u-col u-col-6 toppicks__box'>
-          <div className='toppicks__box-content'>
-            <a href='#'>
-              <img src='http://placehold.it/720x520' alt='' className='toppicks__img' />
-              <div className='toppicks__content-desc'>
-                <div className='title u-truncate'>Munafie Slimming Pants</div>
-                <div className='startto u-truncate'>Mulai dari <span className='price'>Rp. 350rb</span></div>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div className='u-col u-col-6 toppicks__box'>
-          <div className='toppicks__box-content'>
-            <a href='#'>
-              <img src='http://placehold.it/720x520' alt='' className='toppicks__img' />
-              <div className='toppicks__content-desc'>
-                <div className='title u-truncate'>Munafie Slimming Pants</div>
-                <div className='startto u-truncate'>Mulai dari <span className='price'>Rp. 350rb</span></div>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div className='toppicks__see-all'>
-        <a className='toppicks__see-all-link' href='https://www.tokopedia.com/toppicks/'>
-          Lihat semua <i className='promo-spacer__icon promo-spacer__icon--arrow' />
-        </a>
       </div>
     </div>
   </div>
