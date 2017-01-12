ac0ef56b9fdea44873d024447b86c8de99c1b1e9
Kelvin Mijaya <kelvin.mijaya@tokopedia.com>
2017-01-12 19:28:16 +0700

    add touch effect

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -5,6 +5,7 @@ import './Tabs.scss'
 import Tab from './Tab.js'
 import TabContent from './TabContent'
 
+let axis = 0
 class Tabs extends Component {
   static propTypes = {
     children: React.PropTypes.node,
@@ -34,6 +35,8 @@ class Tabs extends Component {
     this.renderContents = this.renderContents.bind(this)
     this.renderHeaders = this.renderHeaders.bind(this)
     this.detectScroll = this.detectScroll.bind(this)
+    this.ontouch = this.ontouch.bind(this)
+    this.loadTouch = this.loadTouch.bind(this)
     // this.updatePointer = this.updatePointer.bind(this)
   }
 
@@ -41,7 +44,7 @@ class Tabs extends Component {
     let prev = document.getElementById('prev')
     let next = document.getElementById('next')
     let inner = window.innerWidth
-    const slideTrackEl = document.querySelector('#loggedin-tab .tab')
+    const slideTrackEl = document.querySelector('#loggedin-tab #slick-track')
     const vpWidth = Math.max(document.documentElement.clientWidth, inner || 0)
 
     let widthTabs = 0
@@ -73,6 +76,84 @@ class Tabs extends Component {
     window.addEventListener('resize', this.handleResize)
     window.addEventListener('scroll', this.detectScroll)
     this.handleResize()
+    window.addEventListener('load', this.loadTouch)
+    this.loadTouch()
+  }
+
+  loadTouch () {
+    var velo = 1
+    var el = document.getElementById('slick-track')
+    let inner = window.innerWidth
+    const vpWidth = Math.max(document.documentElement.clientWidth, inner || 0)
+    const widthTabs = document.getElementById('slick-track').offsetWidth
+    const maxTranslateX = -1 * (widthTabs - vpWidth)
+    this.ontouch(el, function (evt, dir, phase, swipetype, distance) {
+      const slideTrackEl = document.querySelector('#loggedin-tab #slick-track')
+      if (axis + (distance * velo) <= 0 &&
+      axis + (distance * velo) >= maxTranslateX) {
+        axis = axis + (distance * velo)
+        slideTrackEl.style.transform = `translate3d(${axis}px,
+          0px, 0px)`
+      }
+    }, maxTranslateX)
+  }
+  ontouch (el, callback) {
+    var touchsurface = el,
+      dir,
+      swipeType,
+      startX,
+      startY,
+      distX,
+      distY,
+      threshold = 150,
+      restraint = 100,
+      allowedTime = 500,
+      elapsedTime,
+      dist,
+      startTime,
+      handletouch = callback ||
+    function (evt, dir, phase, swipetype, distance) {}
+
+    touchsurface.addEventListener('touchstart', function (e) {
+      var touchobj = e.changedTouches[0]
+      dir = 'none'
+      swipeType = 'none'
+      dist = 0
+      startX = touchobj.pageX
+      startY = touchobj.pageY
+      startTime = new Date().getTime()
+      handletouch(e, 'none', 'start', swipeType, 0)
+      e.preventDefault()
+    }, false)
+
+    touchsurface.addEventListener('touchmove', function (e) {
+      var touchobj = e.changedTouches[0]
+      distX = touchobj.pageX - startX
+      distY = touchobj.pageY - startY
+      if (Math.abs(distX) > Math.abs(distY)) {
+        dir = (distX < 0) ? 'left' : 'right'
+        handletouch(e, dir, 'move', swipeType, distX)
+      } else {
+        dir = (distY < 0) ? 'up' : 'down'
+        handletouch(e, dir, 'move', swipeType, distY)
+      }
+      e.preventDefault()
+    }, false)
+
+    touchsurface.addEventListener('touchend', function (e) {
+      // var touchobj = e.changedTouches[0]
+      elapsedTime = new Date().getTime() - startTime // get time elapsed
+      if (elapsedTime <= allowedTime) {
+        if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+          swipeType = dir // set swipeType to either "left" or "right"
+        } else if (Math.abs(distY) >= threshold &&
+          Math.abs(distX) <= restraint) {
+          swipeType = dir // set swipeType to either "top" or "down"
+        }
+      }
+      handletouch(e, dir, 'end', swipeType, (dir === 'left' || dir === 'right') ? distX : distY)
+      e.preventDefault()
+    }, false)
   }
 
   componentWillUnmount () {
@@ -160,11 +241,13 @@ class Tabs extends Component {
         <button type='button' onClick={() => this.generateAfterChange('next')} id='next' className='slick-arrow slick-next'>
          Next
         </button>
-        <nav ref={this.initNavigationNode} className='tab'>
-          <span id='slick-track'>
-            {this.renderHeaders(headers)}
-          </span>
-        </nav>
+        <div className='slider'>
+          <nav ref={this.initNavigationNode} className='tab'>
+            <span id='slick-track'>
+              {this.renderHeaders(headers)}
+            </span>
+          </nav>
+        </div>
         {/* <span className='tab__pointer' style={this.state.pointer} /> */}
         {this.renderContents(contents)}
       </div>
