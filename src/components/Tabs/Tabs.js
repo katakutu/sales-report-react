0f5e6ea879f041fe69ed55ccbe312f7263d7dab3
darrylsepeda <darryl@thereal-website.com>
2017-01-12 19:36:32 +0700

    slick 3 fast but stiff

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -4,7 +4,9 @@ import classnames from 'classnames'
 import './Tabs.scss'
 import Tab from './Tab.js'
 import TabContent from './TabContent'
-
+let xDown = null;                                                        
+let yDown = null; 
+let location = 0;
 class Tabs extends Component {
   static propTypes = {
     children: React.PropTypes.node,
@@ -34,14 +36,14 @@ class Tabs extends Component {
     this.renderContents = this.renderContents.bind(this)
     this.renderHeaders = this.renderHeaders.bind(this)
     this.detectScroll = this.detectScroll.bind(this)
-    // this.updatePointer = this.updatePointer.bind(this)
+    this.componentWillReceiveProps = this.componentWillReceiveProps.bind(this)
   }
 
   generateAfterChange (direction) {
     let prev = document.getElementById('prev')
     let next = document.getElementById('next')
     let inner = window.innerWidth
-    const slideTrackEl = document.querySelector('#loggedin-tab .tab')
+    const slideTrackEl = document.querySelector('#loggedin-tab #slick-track')
     const vpWidth = Math.max(document.documentElement.clientWidth, inner || 0)
 
     let widthTabs = 0
@@ -59,8 +61,70 @@ class Tabs extends Component {
       slideTrackEl.style.transform = `translate3d(0px, 0px, 0px)`
     }
   }
+
+  componentWillReceiveProps() {
+    this.detectScroll()
+  }
+                                                      
+
+  handleTouchStart(evt) {                                         
+      xDown = evt.touches[0].clientX;                                      
+      yDown = evt.touches[0].clientY;                                      
+  }                                               
+
+  handleTouchMove(evt) {
+      if ( ! xDown || ! yDown ) {
+          return;
+      }
+
+      var xUp = evt.touches[0].clientX;                                    
+      var yUp = evt.touches[0].clientY;
+
+      var xDiff = xDown - xUp;
+      var yDiff = yDown - yUp;
+      console.log(xDiff)
+      const slideTrackEl = document.getElementById('slick-track')
+      let inner = window.innerWidth
+      // const maxTranslateX = (widthTabs - vpWidth)
+      // let value = window.getComputedStyle(slideTrackEl).getPropertyValue('transform');
+      // const value2 = value.split(', ');
+      // console.log(window.getComputedStyle(slideTrackEl).getPropertyValue('transform'));
+      // console.log(value2[4]+', '+(xDiff/2)+' = '+maxTranslateX);
+      // if(xDiff<0) { xDiff = 0; }
+      if(location + (-1 * xDiff) > (inner/5) || location + (-1 * xDiff) < (-1 * (inner/2))){
+
+      }else{
+        location += (-1 * xDiff/2)
+      }
+      
+
+      slideTrackEl.style.transform = `translate3d(${location}px, 0px, 0px)`
+      // if(maxTranslateX < 0){
+      //   maxTranslateX = 0;
+      // }
+     
+      // console.log(window.getComputedStyle(slideTrackEl,null).getPropertyValue('transform'));
+      // if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+      //     if ( xDiff > 0 ) {
+      //         /* left swipe */ 
+      //     } else {
+      //         /* right swipe */
+      //     }                       
+      // } else {
+      //     if ( yDiff > 0 ) {
+      //         /* up swipe */ 
+      //     } else { 
+      //         /* down swipe */
+      //     }                                                                 
+      // }
+      // /* reset values */
+      // xDown = null;
+      // yDown = null;                                             
+  }
+
   detectScroll () {
     var header = document.getElementsByTagName('header')[0].className
+    console.log(header);
     if (header.indexOf('transform') >= 0) {
       document.getElementById('next').classList.add('mini')
       document.getElementById('prev').classList.add('mini')
@@ -72,6 +136,8 @@ class Tabs extends Component {
   componentDidMount () {
     window.addEventListener('resize', this.handleResize)
     window.addEventListener('scroll', this.detectScroll)
+    window.addEventListener('touchstart', this.handleTouchStart)
+    window.addEventListener('touchmove', this.handleTouchMove)
     this.handleResize()
   }
 
@@ -160,11 +226,13 @@ class Tabs extends Component {
         <button type='button' onClick={() => this.generateAfterChange('next')} id='next' className='slick-arrow slick-next'>
          Next
         </button>
-        <nav ref={this.initNavigationNode} className='tab'>
-          <span id='slick-track'>
-            {this.renderHeaders(headers)}
-          </span>
-        </nav>
+        <div className="slider">
+          <nav ref={this.initNavigationNode} className='tab'>
+            <span id='slick-track'>
+              {this.renderHeaders(headers)}
+            </span>
+          </nav>
+        </div>
         {/* <span className='tab__pointer' style={this.state.pointer} /> */}
         {this.renderContents(contents)}
       </div>
