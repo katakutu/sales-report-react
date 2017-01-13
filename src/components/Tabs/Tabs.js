6f56f95dba72cd5bfa1314842bd8f99b9c2e500a
Kelvin Mijaya <kelvin.mijaya@tokopedia.com>
2017-01-13 10:59:13 +0700

    fixing prevent default

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -5,7 +5,6 @@ import './Tabs.scss'
 import Tab from './Tab.js'
 import TabContent from './TabContent'
 
-let axis = 0 // define global for axis touch
 class Tabs extends Component {
   static propTypes = {
     children: React.PropTypes.node,
@@ -72,15 +71,9 @@ class Tabs extends Component {
       document.getElementById('prev').classList.remove('mini')
     }
   }
-  componentDidMount () {
-    window.addEventListener('resize', this.handleResize)
-    window.addEventListener('scroll', this.detectScroll)
-    this.handleResize()
-    window.addEventListener('load', this.loadTouch)
-    this.loadTouch()
-  }
 
   loadTouch () {
+    let axis = 0 // define global for axis touch
     var velo = 0.8, range = 50
     let ele = document.getElementById('slick-track')
     let inner = window.innerWidth
@@ -96,13 +89,14 @@ class Tabs extends Component {
           0px, 0px)`
       }
       if (phase === 'end') {
+        let tamp
         if (axis >= 0) {
-          slideTrackEl.style.transform = `translate3d(${0}px,
-            0px, 0px)`
+          tamp = 0
         } else if (axis <= maxTranslateX) {
-          slideTrackEl.style.transform = `translate3d(${maxTranslateX}px,
-            0px, 0px)`
+          tamp = maxTranslateX
         }
+        slideTrackEl.style.transform = `translate3d(${tamp}px,
+          0px, 0px)`
       }
     })
   }
@@ -134,7 +128,7 @@ class Tabs extends Component {
       startY = touchobj.pageY
       startTime = new Date().getTime()
       handletouch(e, 'none', 'start', swipeType, 0)
-      e.preventDefault()
+      // e.preventDefault()
     }, false)
 
     touchsurface.addEventListener('touchmove', function (e) {
@@ -148,7 +142,7 @@ class Tabs extends Component {
         dir = (distY < 0) ? 'up' : 'down'
         handletouch(e, dir, 'move', swipeType, distY)
       }
-      e.preventDefault()
+      // e.preventDefault()
     }, false)
 
     touchsurface.addEventListener('touchend', function (e) {
@@ -162,8 +156,15 @@ class Tabs extends Component {
         }
       }
       handletouch(e, dir, 'end', swipeType, (dir === 'left' || dir === 'right') ? distX : distY)
-      e.preventDefault()
+      // e.preventDefault()
     }, false)
+  }
+
+  componentDidMount () {
+    window.addEventListener('resize', this.handleResize)
+    window.addEventListener('scroll', this.detectScroll)
+    window.addEventListener('load', this.loadTouch())
+    this.handleResize()
   }
 
   componentWillUnmount () {
