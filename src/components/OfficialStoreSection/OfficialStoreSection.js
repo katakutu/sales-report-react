93566e0a3e25e6345ee02e88c0fb2c897b4247b5
Alex Xandra Albert Sim <bertzzie@gmail.com>
2017-01-17 15:07:25 +0700

    Various polish
    - Added GTM events on category and official store
    - Search wishlist widget always shown (even when there's no result)

--- src/components/OfficialStoreSection/OfficialStoreSection.js
+++ src/components/OfficialStoreSection/OfficialStoreSection.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react'
 import './OfficialStoreSection.scss'
+
 import TextHeader from '../../components/TextHeader'
+import GTM from '../../lib/utils/GTM'
 
 class OfficialStoreSection extends Component {
   static propTypes = {
     data: React.PropTypes.arrayOf(React.PropTypes.object)
   }
 
+  constructor (props) {
+    super(props)
+
+    this._gtmNotifyOfficialStoreClicked = this._gtmNotifyOfficialStoreClicked.bind(this)
+  }
+
+  _gtmNotifyOfficialStoreClicked (item) {
+    return (event) => {
+      const label = `Official Store - ${item['name']}`
+      GTM.pushEvent('clickOfficialStore', 'Homepage', 'Click', label)
+    }
+  }
+
   renderStore (data, index) {
     return (
       <div className='u-col u-col-3 official-store__item' key={`os-${index}`}>
         <a href={data.url}
+          onClick={this._gtmNotifyOfficialStoreClicked(data)}
           data-value={data.name}
           className='official-store__link u-center'>
           <div className='u-table'>
