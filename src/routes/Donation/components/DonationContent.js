936ac5551cfe6bf0c414a73fc36d1d240f0b217e
NikodemusLimanthie <nikodemus.limanthie@tokopedia.com>
2017-01-20 09:57:04 +0700

    Make interaction for donation lembaga content

--- src/routes/Donation/components/DonationContent.js
+++ src/routes/Donation/components/DonationContent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { browserHistory, Link } from 'react-router'
+import classNames from 'classnames'
 
 import './DonationView.scss'
 import BaznasLogo from '../assets/lembaga/baznas1.png'
@@ -8,6 +9,22 @@ import PkpuLogo from '../assets/lembaga/pkpu.png'
 import YcabLogo from '../assets/lembaga/ycab1.png'
 
 class DonationContent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeContent: 'baznas'
+    }
+  }
+
+  handleContentChange(name) {
+    this.setState({
+      activeContent: name
+    })
+  }
+
+  handleDropdownClick(e) {
+  }
+
   render() {
     return (
       <div className='u-clearfix donasi-content'>
@@ -22,22 +39,22 @@ class DonationContent extends Component {
                 <label className='u-inline-block'>Pilih Lembaga Donasi</label>
                 <div><small className='u-h5 error-input u-display-none u-inline-block ' id='error-client-number'>Anda belum memilih lembaga donasi</small></div>
                 <ul className='u-clearfix u-mt0 u-mb0 u-pl0 u-list-style-none lembaga-logo-list'>
-                  <li>
+                  <li onClick={this.handleContentChange.bind(this, 'baznas')}>
                     <div className='lembaga-tab-logo'>
                       <img src={BaznasLogo} alt='' />
                     </div>
                   </li>
-                  <li>
+                  <li onClick={this.handleContentChange.bind(this, 'ycab')}>
                     <div className='lembaga-tab-logo'>
                       <img src={YcabLogo} alt='' />
                     </div>
                   </li>
-                  <li>
+                  <li onClick={this.handleContentChange.bind(this, 'dompetdhuafa')}>
                     <div className='lembaga-tab-logo'>
                       <img src={DompetDuafaLogo} alt='' />
                     </div>
                   </li>
-                  <li>
+                  <li onClick={this.handleContentChange.bind(this, 'pkpu')}>
                     <div className='lembaga-tab-logo'>
                       <img src={PkpuLogo} alt='' />
                     </div>
@@ -46,14 +63,47 @@ class DonationContent extends Component {
               </div>
 
               <div className='lembaga-tab-content'>
-                <div className='u-clearfix u-relative lembaga-content'>
+                <div className={classNames('u-clearfix', 'u-relative', 'lembaga-content', {'u-hide': this.state.activeContent !== 'baznas'})}>
                   <div className='u-relative u-mb2 u-center lembaga-content-img'>
                     <img src={BaznasLogo} alt='' className='u-align-middle' />
                   </div>
                   <div className='lembaga-content-text'>
                     <p className='u-h5 u-mt0 u-mb2'>Badan Amil Zakat Nasional (BAZNAS) merupakan lembaga yang di bentuk pemerintah melalui KEPPRES No. 8 Tahun 2001 yang memiliki fungsi penghimpunan dan penyaluran zakat, infak, dan sedekah (ZIS) sesuai undang-undang nomor 23 tahun 2011 tentang pengelolaan zakat.</p>
                     <p className='u-h5 u-mt0 u-mb0'>
-                      <Link>Lihat selengkapnya ›</Link>
+                      <Link to='http://www.baznas.go.id/'>Lihat selengkapnya ›</Link>
+                    </p>
+                  </div>
+                </div>
+                <div className={classNames('u-clearfix', 'u-relative', 'lembaga-content', {'u-hide': this.state.activeContent !== 'ycab'})}>
+                  <div className='u-relative u-mb2 u-center lembaga-content-img'>
+                    <img src={YcabLogo} alt='' className='u-align-middle' />
+                  </div>
+                  <div className='lembaga-content-text'>
+                    <p className='u-h5 u-mt0 u-mb2'>YCAB Foundation (Yayasan Cinta Anak Bangsa) adalah sebuah organisasi non-profit yang berfokus pada menciptakan kemandirian bagi anak muda melalui pendidikan, pemberdayaan ekonomi dan promosi gaya hidup sehat.</p>
+                    <p className='u-h5 u-mt0 u-mb0'>
+                      <Link to='http://www.ycabfoundation.org/'>Lihat selengkapnya ›</Link>
+                    </p>
+                  </div>
+                </div>
+                <div className={classNames('u-clearfix', 'u-relative', 'lembaga-content', {'u-hide': this.state.activeContent !== 'dompetdhuafa'})}>
+                  <div className='u-relative u-mb2 u-center lembaga-content-img'>
+                    <img src={DompetDuafaLogo} alt='' className='u-align-middle' />
+                  </div>
+                  <div className='lembaga-content-text'>
+                    <p className='u-h5 u-mt0 u-mb2'>Dompet Dhuafa adalah Lembaga Nirlaba milik masyarakat yang berkhidmat mengangkat harkat sosial kemanusiaan dengan mendayagunakan Zakat, Infak, Sedekah dan Wakaf (ZISWAF) serta dana sosial lainnya baik dari individu, kelompok maupun perusahaan.</p>
+                    <p className='u-h5 u-mt0 u-mb0'>
+                      <Link to='http://www.dompetdhuafa.org/'>Lihat selengkapnya ›</Link>
+                    </p>
+                  </div>
+                </div>
+                <div className={classNames('u-clearfix', 'u-relative', 'lembaga-content', {'u-hide': this.state.activeContent !== 'pkpu'})}>
+                  <div className='u-relative u-mb2 u-center lembaga-content-img'>
+                    <img src={PkpuLogo} alt='' className='u-align-middle' />
+                  </div>
+                  <div className='lembaga-content-text'>
+                    <p className='u-h5 u-mt0 u-mb2'>PKPU adalah sebuah badan hukum berbentuk yayasan yang bersifat sosial dan kemanusiaan dan menjalankan kegiatan usahanya dalam bidang pengelolaan dana CSR dan donasi-donasi tidak mengikat lainnya.</p>
+                    <p className='u-h5 u-mt0 u-mb0'>
+                      <Link to='http://www.pkpu.or.id/'>Lihat selengkapnya ›</Link>
                     </p>
                   </div>
                 </div>
@@ -66,7 +116,7 @@ class DonationContent extends Component {
               <div className='form-group nominal u-mb2 u-block'>
                 <label className='u-mb1'>Nominal</label>
                 <div class='donasi-select'>
-                  <select className='donasi-select form-control form-select'>
+                  <select className='donasi-select form-control form-select' onClick={this.handleDropdownClick.bind(this)}>
                     <option value='25000'>Rp 25.000</option>
                     <option value='50000'>Rp 50.000</option>
                     <option value='100000'>Rp 100.000</option>
--- src/routes/Donation/components/DonationSelectDrawer.js
+++ src/routes/Donation/components/DonationSelectDrawer.js
@@ -9,7 +9,7 @@ class DonationSelectDrawer extends Component {
     super(props);
 
     this.state = {
-      open: false
+      open: true
     }
   }
 
