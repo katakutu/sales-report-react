9a205166839a401d680bcf03f44451d86facf0f8
Nikhil Ranjan <nikhil.ranjan@tokopedia.com>
2017-01-23 14:51:04 +0700

    Revert "Added precache from service worker for cdn"
    This reverts commit f1f58e41d080445978a1fba70cddadbe6b9143cd.

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -70,11 +70,7 @@ webpackConfig.plugins = [
   }),
   new SWPrecacheWebpackPlugin({
     cacheId: 'toped-lite-v1',
-    filename: 'service-worker.js',
-    runtimeCaching: [{
-      urlPattern: /^https:\/\/ecs7\.tokopedia\.net\/assets-tokopedia-lite/,
-      handler: 'fastest'
-    }]
+    filename: 'service-worker.js'
   })
 ]
 
