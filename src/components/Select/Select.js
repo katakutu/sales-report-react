933b8aa8c06e01e4d995ecf410a2a8d71cee7533
Arian Pradana <arian.raptor@gmail.com>
2017-01-25 14:42:34 +0700

    commit

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -6,6 +6,7 @@ class Select extends Component {
   static propTypes = {
     children: React.PropTypes.node,
     className: React.PropTypes.string,
+    id: React.PropTypes.string,
     onClick: React.PropTypes.func
   }
 
@@ -16,7 +17,7 @@ class Select extends Component {
   render () {
     return (
       <div>
-        <select name='' id='' className='u-hide'>
+        <select name='' id={this.props.id} className='u-hide'>
           {this.props.children}
         </select>
         <div className='select'>
