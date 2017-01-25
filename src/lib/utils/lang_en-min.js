26114d1b29a902f40c8b6f6462b3b23c8dd05cb8
dendisunardi <dendi.sunardi@gmail.com>
2017-01-25 17:13:08 +0700

    fixing conflict on lang

--- src/lib/utils/lang_en-min.js
+++ src/lib/utils/lang_en-min.js
@@ -99,7 +99,7 @@ const lang = {
     'Wishlist Search Empty': 'No matching product found',
     'See All Wishlists': 'See All Wishlists',
     'Search Shop in Favorite': 'Search shops in favorite list',
-    'Cari lokasi': 'Search Location',
+    'Cari lokasi': 'Search Location'
     'TERAKHIR DILIHAT': 'RECENTLY VIEWED',
     'INSPIRASI DARI MINAT ANDA' :'RELATED TO ITEMS YOU\'VE VIEWED',
     'INSPIRASI DARI WISHLIST' : 'INSPIRED BY YOUR WISHLIST',
