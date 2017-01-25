cb54db951a891ef3b4a4f25faffcb5030a9fc822
NikodemusLimanthie <nikodemus.limanthie@tokopedia.com>
2017-01-25 16:16:17 +0700

    Fix lint

--- src/routes/Digital/components/DigitalProductRadioButtons.js
+++ src/routes/Digital/components/DigitalProductRadioButtons.js
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react'
-import classNames from 'classnames'
 import './DigitalProductRadioButtons.scss'
 
 class DigitalProductRadioButtons extends Component {
@@ -11,12 +10,12 @@ class DigitalProductRadioButtons extends Component {
   render () {
     return (
       <div>
-        {this.props.buttons.map(function(button, index) {
+        {this.props.buttons.map(function (button, index) {
           return (
             <div className='dp-radio'>
               <input type='radio' id={button.id} name={button.name} className='u-hide' checked={button.checked} />
               <label htmlFor={button.id} className='dp-radio__label'>
-                <span className='dp-radio__icon'></span>
+                <span className='dp-radio__icon' />
                 <span className='dp-radio__text'>{button.text}</span>
               </label>
             </div>
