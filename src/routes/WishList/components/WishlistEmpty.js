cdbabe7e98dab4a927f5172fc7cf5b9ff7890483
dendisunardi <dendi.sunardi@gmail.com>
2017-01-27 16:37:11 +0700

    fixing tollerance area on empty wishlish

--- src/routes/WishList/components/WishlistEmpty.js
+++ src/routes/WishList/components/WishlistEmpty.js
@@ -75,11 +75,13 @@ class WishlistEmpty extends Component {
         <div className='wishlist__empty'>
           { lang[this.props.lang]['Empty wishlist'] }
         </div>
-        <a href={`${HOSTNAME}/toppicks`} className='wishlist__btn-holder'>
-          <div className='wishlist__lets-search'>
-            { lang[this.props.lang]['Search for Products'] }
-          </div>
-        </a>
+        <div className='wishlist__btn-holder'>
+          <a href={`${HOSTNAME}/toppicks`}>
+            <div className='wishlist__lets-search'>
+              { lang[this.props.lang]['Search for Products'] }
+            </div>
+          </a>
+        </div>
         <TopAds
           userID={this.props.userID}
           ep={TOPADS_PARAMS.ep}
