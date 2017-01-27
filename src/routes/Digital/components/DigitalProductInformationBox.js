551e6c8549109bdeacfcb51556c0c49759a19eee
Arian Pradana <arian.raptor@gmail.com>
2017-01-27 11:26:16 +0700

    Create custom color of information box

--- src/routes/Digital/components/DigitalProductInformationBox.js
+++ src/routes/Digital/components/DigitalProductInformationBox.js
@@ -5,22 +5,31 @@ class DigitalProductInformationBox extends Component {
 
   static propTypes = {
     isTokenListrik: PropTypes.bool,
-    content: PropTypes.string
+    content: PropTypes.string,
+    withTitle: PropTypes.bool,
+    title: PropTypes.string,
+    boxType: PropTypes.string
+  }
+
+  static defaultProps = {
+    withTitle: true,
+    title: 'Keterangan',
+    boxType: 'green'
   }
 
   render () {
 
     if(this.props.isTokenListrik) {
       return (
-        <div className='dp-informationbox u-mb1 u-border-box'>
-          <p className='dp-informationbox__title u-h4'>Keterangan</p>
+        <div className={'dp-informationbox u-mb1 u-border-box dp-informationbox--' + this.props.boxType}>
+          <p className='dp-informationbox__title u-h4'>{this.props.title}</p>
           <p className='dp-informationbox__content u-h4 u-m0'>{this.props.content}</p>
         </div>
       )
     } else {
       return (
-        <div className='dp-informationbox u-mb1 u-border-box'>
-          <p className='dp-informationbox__title u-h4'>Keterangan</p>
+        <div className={'dp-informationbox u-mb1 u-border-box dp-informationbox--' + this.props.boxType}>
+          <p className='dp-informationbox__title u-h4'>{this.props.title}</p>
           <p className='dp-informationbox__content u-h4 u-m0'>{this.props.content.map(function (item, index) {
             return (
               <div key={index}>{item.text} <br /></div>
