8c9b1aa1b5013078a46e2b5f9651743bb391a1f5
Alex Xandra Albert Sim <bertzzie@gmail.com>
2017-02-01 10:30:58 +0700

    Scrolling fix on header

--- src/components/HeaderHomeOld/HeaderTab.js
+++ src/components/HeaderHomeOld/HeaderTab.js
@@ -39,25 +39,24 @@ class HeaderTab extends Component {
 
   _savePosition (val, query = null) {
     const { router } = this.context
-    let currentKey = this.props.activeTab
+    const currentKey = this.props.activeTab
     // get scrolled position
-    let scrollPosition = (window.pageYOffset !== undefined) ? window.pageYOffset
+    const scrollPosition = (window.pageYOffset !== undefined) ? window.pageYOffset
     : (document.documentElement || document.body.parentNode || document.body).scrollTop
     // update scroll history
     let updateState = {}
-    let currentState = this.props.scrollHistory
+    const currentState = this.props.scrollHistory
     // check available
     if (currentState) {
       // update state
-      currentState[currentKey] = { point: scrollPosition }
-      updateState = currentState
+      updateState = Object.assign({}, currentState, { [currentKey]: { point: scrollPosition } })
     } else {
       updateState[currentKey] = { point: scrollPosition }
     }
     // update to store
     this.props.updateScrollPosition(updateState)
     // push location state
-    let queries = query && query
+    const queries = query && query
     router.push({
       pathname: val,
       query: queries
