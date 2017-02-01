1ad6f2b07d858d870924cf671ed4b8df7104ca92
Alex Xandra Albert Sim <bertzzie@gmail.com>
2017-02-01 09:35:29 +0700

    Redirect to feed preview turned on again

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -3,19 +3,19 @@ import HomeView from './components/HomeView'
 // Sync route definition
 export default (store, ApolloExecutors) => {
   return {
-    component: HomeView
-    /* onEnter: (nextState, replace, callback) => {
+    component: HomeView,
+    onEnter: (nextState, replace, callback) => {
       return ApolloExecutors.isUserLoggedIn()
         .then(isUserLoggedIn => {
           if (nextState.location.pathname === '/' &&
              !nextState.location.query.h &&
              isUserLoggedIn) {
-            window.location = `/feed`
+            window.location = `/?view=feed_preview`
           }
 
           return callback()
         })
         .catch(err => callback(err))
-    } */
+    }
   }
 }
